e6d11915ff599450e9ab3849f95151c079258432
Marshall-Bits <kz_@hotmail.es>
2022-04-12 18:12:19 +0200

    adding animation 1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 const hamburgerButton = document.getElementById('hamburger');
 const navList = document.getElementById('nav-list');
+const aboutMeButton = document.getElementById('about-me-button')
+const projectsButton = document.getElementById('projects-button')
 
 function toggleButton(){
     navList.classList.toggle('show')
 }
 
 hamburgerButton.addEventListener('click', toggleButton)
+
+function scrollToAboutMe(){
+    document.getElementById('about-me').scrollIntoView({
+        behavior: 'smooth'
+      });
+}
+
+aboutMeButton.addEventListener('click', scrollToAboutMe)
+
+function scrollToProjects(){
+    document.getElementById('projects').scrollIntoView({
+        behavior: 'smooth'
+      });
+}
+
+projectsButton.addEventListener('click', scrollToProjects)
